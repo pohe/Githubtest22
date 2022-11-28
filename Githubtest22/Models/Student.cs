edcc6b2eb048084098ea628175b3fc0df6049ab1
Poul Henriksen <pohe@easj.dk>
2022-11-28 10:43:52 +0100

    added kommentar

--- Githubtest22/Models/Student.cs
+++ Githubtest22/Models/Student.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
 
-        public Student(int id)
+        public Student(int id) //jeg har lavet en name parameter
         {
             Id = id;
         }
