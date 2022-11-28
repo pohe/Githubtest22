aced121ab9a5e2ab43f90cab788afcc28758fc33
Poul Henriksen <pohe@easj.dk>
2022-11-28 10:35:49 +0100

    added name parameter

--- Githubtest22/Models/Student.cs
+++ Githubtest22/Models/Student.cs
@@ -5,9 +5,9 @@
         public int Id { get; set; }
         public string Name { get; set; }
 
-        public Student()
+        public Student(string name)
         {
-
+            Name = name; 
         }
 
         public override string ToString()
